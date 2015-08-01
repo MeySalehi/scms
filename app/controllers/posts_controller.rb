4e2f30ccd88a61565a9bc55834224a6a0033060b
Meysam Salehi <meysam_salehi@outlook.com>
2015-08-01 15:15:01 +0430

    define posts_controller structure

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,38 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.recent_post(5)
   end
 
   def page
+
+    @posts = Post.page(params[:id].to_i,2)
   end
 
   def show
+    p = params[:id]
+    @post = Post.find_by_permalink(p)
+
+    unless @post
+      render file: "#{Rails.root}/public/404.html"
+    end
   end
 
   def category
+    category = Category.find_by_name(params[:id])
+    @posts = category.posts
+
+    if @posts == []
+      render file: "#{Rails.root}/public/404.html"
+    end
   end
 
   def search
+    @posts = Post.search(params[:id]).reverse
+
+    if @posts == []
+      render file: "#{Rails.root}/public/404.html"
+    end
+
   end
 
   def comment
