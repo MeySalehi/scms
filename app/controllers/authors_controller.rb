4dd2b763f17574c5efd99e4de009e7bdc3cb4798
Meysam Salehi <meysam_salehi@outlook.com>
2015-08-01 15:11:31 +0430

    define authors_controller structure

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,11 @@
 class AuthorsController < ApplicationController
   def index
+  	@authors = User.order("full_name asc")
   end
 
   def profile
+  	@author = User.find_by_username(params[:username])
+  	@author_posts = @author.posts
   end
 end
+#we need to add 2 column in user table: 1. public_email 2.porofile_visible
