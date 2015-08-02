e3be0f52654e72412a0718295b769a94deb026e7
Meysam Salehi <meysam_salehi@outlook.com>
2015-08-02 11:00:08 +0430

    generate Uploads controller in admin namespace

---
+++ app/controllers/admin/uploads_controller.rb
@@ -0,0 +1,2 @@
+class Admin::UploadsController < ApplicationController
+end
---
+++ test/controllers/admin/uploads_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::UploadsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
