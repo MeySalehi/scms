e51f392e6d6bbf17d72d3aea3ec60867f3b5f23f
Meysam Salehi <meysam_salehi@outlook.com>
2015-08-04 22:23:01 +0430

    edit authors_controller.rb for use page action

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,11 +1,20 @@
 class AuthorsController < ApplicationController
   def index
-  	@authors = User.order("full_name asc")
+	  @authors = User.list(1 , 4)
   end
 
+  def page
+  	page = params[:id].to_i
+		
+		if page == ( 0 or nil )
+	  	redirect_to :controller => "authors", :action => "index"
+	  else
+	  	@authors = User.list(page, 4)
+	  end
+  end
+  
   def profile
   	@author = User.find_by_username(params[:username])
   	@author_posts = @author.posts
   end
 end
-#we need to add 2 column in user table: 1. public_email 2.porofile_visible
