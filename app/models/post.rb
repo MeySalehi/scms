a6362c94ebb5594e9d1ae3b57630c944a9f9e066
Meysam Salehi <meysam_salehi@outlook.com>
2015-08-05 16:25:56 +0430

    new scopes added to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,11 @@ class Post < ActiveRecord::Base
 	has_many	:comments
 	has_and_belongs_to_many :categories, :join_table => :posts_categories
 
-	scope :recent_post, lambda { |num| last(num).reverse }
-	scope :search, lambda { |query| where(["title LIKE ?", "%#{query}%" ]) }
-	scope :page, lambda { |page, limit| limit(limit).last((limit * page)).first(limit).reverse} 
+	scope :post_by_permalink, lambda {|permalink| where(permalink: permalink, type: "POST" ).first}
+	scope :page_by_permalink, lambda {|permalink| where(permalink: permalink, type: "PAGE" ).first}
+	scope :recent_post,		lambda { |num| last(num).reverse }
+	scope :search,			lambda { |query| where(["title LIKE ?", "%#{query}%" ]) }
+	scope :page,			lambda { |page, limit| limit(limit).last((limit * page)).first(limit).reverse}
+	scope :page_count,		lambda { where(:type, "PAGE").count }
+	scope :post_count,		lambda { where(:type, "POST").count }
 end
