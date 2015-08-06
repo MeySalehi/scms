57a887ecbeaa5114f9a9d41dab7a3a5024fbab69
Meysam Salehi <meysam_salehi@outlook.com>
2015-08-06 11:53:38 +0430

    add more scopes in post.rb model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,11 +4,13 @@ class Post < ActiveRecord::Base
 	has_many	:comments
 	has_and_belongs_to_many :categories, :join_table => :posts_categories
 
-	scope :post_by_permalink, lambda {|permalink| where(permalink: permalink, type: "POST" ).first}
-	scope :page_by_permalink, lambda {|permalink| where(permalink: permalink, type: "PAGE" ).first}
-	scope :recent_post,		lambda { |num| last(num).reverse }
-	scope :search,			lambda { |query| where(["title LIKE ?", "%#{query}%" ]) }
-	scope :page,			lambda { |page, limit| limit(limit).last((limit * page)).first(limit).reverse}
-	scope :page_count,		lambda { where(:type, "PAGE").count }
-	scope :post_count,		lambda { where(:type, "POST").count }
+	scope :post_by_permalink, 	lambda { |permalink| where(permalink: permalink ).first}#<<<< after edit seed add the type: "POST"
+	scope :page_by_permalink, 	lambda { |permalink| where(permalink: permalink ).first}#<<<< after edit seed add the type: "PAGE"
+	scope :public_recent_post,	lambda { |limit| where(status: "PUBLISH").last(limit).reverse }
+	scope :users_recent_post,		lambda { |limit| where(status: ["PUBLISH", "USERS"]).last(limit).reverse }
+	scope :search,							lambda { |query| where(["title LIKE ?", "%#{query}%" ]) }
+	scope :page_public,					lambda { |page, limit| where(status: "PUBLISH").limit(limit).last((limit * page)).first(limit).reverse}
+	scope :page_users,					lambda { |page, limit| where(status: [ "USERS", "PUBLISH"]).limit(limit).last((limit * page)).first(limit).reverse}
+	scope :pages_count,					lambda { where(type_set: "PAGE").count }
+	scope :posts_count,					lambda { where(type_set: "POST").count }
 end
