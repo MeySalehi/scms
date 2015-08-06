18cf7aeb1973376d04b164b8fba2035ddea0c48e
Meysam Salehi <meysam_salehi@outlook.com>
2015-08-06 11:52:48 +0430

    posts controller improved

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,48 +1,84 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.recent_post(5)
+    if session[:user_id]
+      @posts = Post.users_recent_post(5)
+    else
+      @posts = Post.public_recent_post(5)
+    end
   end
 # --------------------------------
   def page
-
-    @posts = Post.page(params[:id].to_i,2)
+    if session[:user_id]
+      @posts = Post.page_users(params[:id].to_i,10)
+    else
+      @posts = Post.page_public(params[:id].to_i,2)
+    end
   end
 # --------------------------------
   def show
-    
-    @post = Post.post_by_permalink(params[:permalink])
+    post_content = Post.post_by_permalink(params[:permalink])
 
-    unless @post
+    unless post_content
       render file: "#{Rails.root}/public/404.html"
     end
-    @comment = Comment.new
+
+    if session[:user_id]
+      @post = post_content
+      @comment = Comment.new
+    elsif post_content.status == "PUBLISH" 
+      @post = post_content
+      @comment = Comment.new
+    else
+      flash[:access_error] = "You Can't Access to this post, please login."
+    end
   end
 # --------------------------------
   def show_page
-    @page = Post.page_by_permalink(params[:permalink])
+    page_content = Post.page_by_permalink(params[:permalink])
 
-    unless @post
+    unless page_content
       render file: "#{Rails.root}/public/404.html"
     end
-    @comment = Comment.new
+
+    if session[:user_id]
+      @page = page_content
+      @comment = Comment.new
+    elsif page_content.status == "PUBLISH" 
+      @page = page_content
+      @comment = Comment.new
+    else
+      flash[:access_error] = "You Can't Access to this page, please login."
+    end
   end
 # --------------------------------
   def category
-    category = Category.find_by_name(params[:title])
-    @posts = category.posts
+    # need to re difine with data and testing
+    
+    # page = params[:id] != nil ? params[:id].to_i : 1
+    # limit = 3 # set by otions
+    # if session[:user_id]
+    #   @posts = Category.category_for_users(params[:title],page,limit)
+    # else
+    #   @posts = Category.category_for_public(params[:title],page,limit)
+    # end       
 
-    if @posts == []
-      render file: "#{Rails.root}/public/404.html"
-    end
+    # if @posts == []
+    #   render file: "#{Rails.root}/public/404.html"
+    # end
   end
 # --------------------------------
   def search
-    @posts = Post.search(params[:query]).reverse
+    page = params[:id] != nil ? params[:id].to_i : 1
+    limit = 3 # set by otions
+    if session[:user_id]
+      @posts = Post.search_for_users(params[:query],page,limit)
+    else
+      @posts = Post.search_for_public(params[:query],page,limit)
+    end       
 
     if @posts == []
       render file: "#{Rails.root}/public/404.html"
     end
-
   end
 # --------------------------------
   def comment
