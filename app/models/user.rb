646186629b273356d83ca2f0d6c599a897097e96
Meysam Salehi <meysam_salehi@outlook.com>
2015-08-10 16:33:58 +0430

    user.rb model edit and bugs fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ActiveRecord::Base
 	has_many 	:posts
 	has_many	:comments
 
-	scope :list, lambda { |page, limit| where(profile_visible: nil).order("full_name asc").last((limit * page)).first(limit).reverse}
+	scope :list, lambda { |page, limit| where(profile_visible: true).order(:full_name).limit(limit).offset((limit * (page -1)))}
 
 
 	scope :user_posts, lambda { |user_id| where(id: user_id).first.posts}
 
+	scope :users_list, lambda { |page, limit| order(full_name: :desc).limit(limit).offset((limit * (page - 1)))}
 end
