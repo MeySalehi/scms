a48d1b5848c4407a90e7e462482a28dd28fbc1e1
Meysam Salehi <meysam_salehi@outlook.com>
2015-08-20 23:43:45 +0430

    user model edited and Paperclip used for handle avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
 	has_many 	:posts
 	has_many	:comments
 
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "50x50>" }, :default_url => "/avatars/default.gif"
+	
+	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
 	scope :list, lambda { |page, limit| where(profile_visible: true).order(:full_name).limit(limit).offset((limit * (page -1)))}
 
 
