1f00ccd6f597d5e3bfbd0f4e903656a12351c044
Meysam Salehi <meysam_salehi@outlook.com>
2015-08-20 23:39:20 +0430

    PaperClip added and database edited

--- db/migrate/20150726060320_fix_tables.rb
+++ db/migrate/20150726060320_fix_tables.rb
@@ -1,36 +1,38 @@
 class FixTables < ActiveRecord::Migration
   
   def up
-    add_column(:users, :public_email, :string)
+   #  add_column(:users, :public_email, :string)
     add_column(:users, :profile_visible, :boolean)
-  	add_column(:users, :access_level,	:string)
-  	add_column(:users, :email, :string,:null => false, :after => :username, :default => "" )
-  	add_column(:posts, :type_set, :string)
-    rename_column(:users, :displayed_name, :full_name)
-  	rename_column(:posts, :author_id, :user_id)
-    change_column(:comments, :author, :string, null: true)
-    change_column(:comments, :author_email, :string, null: true)
-    remove_column(:posts, :password_digest)
-  	# -------------------------
-  	create_table :posts_categories, :id => false do |t|
-  		t.integer :post_id
-  		t.integer	:category_id
-  	end
-  	add_index :posts_categories, [:post_id, :category_id]
-  	#--------------------------
+   #  add_column(:users, :avatar, :string)
+   #  add_column(:users, :access_level,	:string)
+  	# add_column(:users, :email, :string,:null => false, :after => :username, :default => "" )
+  	# add_column(:posts, :type_set, :string)
+   #  rename_column(:users, :displayed_name, :full_name)
+  	# rename_column(:posts, :author_id, :user_id)
+   #  change_column(:comments, :author, :string, null: true)
+   #  change_column(:comments, :author_email, :string, null: true)
+   #  remove_column(:posts, :password_digest)
+  	# # -------------------------
+  	# create_table :posts_categories, :id => false do |t|
+  	# 	t.integer :post_id
+  	# 	t.integer	:category_id
+  	# end
+  	# add_index :posts_categories, [:post_id, :category_id]
+  	# #--------------------------
   end
 
   def down
-  	drop_table :posts_categories
-    add_column(:posts, :password_digest, :string)
-    change_column(:comments, :author, :string, null: false)
-    change_column(:comments, :author_email, :string, null: false)
-  	rename_column(:posts, :user_id,		:author_id)
-  	rename_column(:users, :full_name,	:displayed_name)
-    remove_column(:posts, :type_set)
-  	remove_column(:users, :email)
-		remove_column(:users, :access_level)
-    remove_column(:users, :public_email)
-    remove_column(:users, :profile_visible)
+  # 	drop_table :posts_categories
+  #   add_column(:posts, :password_digest, :string)
+  #   change_column(:comments, :author, :string, null: false)
+  #   change_column(:comments, :author_email, :string, null: false)
+  # 	rename_column(:posts, :user_id,		:author_id)
+  # 	rename_column(:users, :full_name,	:displayed_name)
+  #   remove_column(:posts, :type_set)
+  # 	remove_column(:users, :email)
+		# remove_column(:users, :access_level)
+  #   remove_column(:users, :public_email)
+  #   remove_column(:users, :profile_visible)
+  #   remove_column(:users, :avatar)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150802050230) do
+ActiveRecord::Schema.define(version: 20150820072333) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -96,6 +96,10 @@ ActiveRecord::Schema.define(version: 20150802050230) do
     t.string   "access_level"
     t.string   "email",                  default: "", null: false
     t.string   "public_email"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.boolean  "profile_visible"
   end
 
