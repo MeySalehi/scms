00c918d05dea75d40ae4608f28cdfe288d75d2d4
Meysam Salehi <meysam_salehi@outlook.com>
2015-08-25 17:37:31 +0430

    nested comments ability added to post view

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+	def comments_tree_for(comments)
+		comments.map do |comment|
+			nested_comments = Comment.childs(comment.id)
+			render(partial: "posts/comment", object: comment) +
+        (nested_comments.size > 0 ? content_tag(:div, comments_tree_for(nested_comments), class: 'replies') : nil)
+		end.join.html_safe
+	end
 end
